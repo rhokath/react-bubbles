504ce8ed05b54d61b1e9fd13142481e3a772c25c
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-08-23 12:32:06 -0400

    mvp

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,73 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+// const Login = () => {
+//   // make a post request to retrieve a token from the api
+//   // when you have handled the token, navigate to the BubblePage route
+//   return (
+//     <>
+//       <h1>Welcome to the Bubble App!</h1>
+//       <p>Build a login page here</p>
+//     </>
+//   );
+// };
+
+import axios from 'axios';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    },
+    isLoading: false
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/protected');
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
 
 export default Login;
